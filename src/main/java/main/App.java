e05b8fa193c79026199056a8aee96b77a252cf8c
mikaelnorppa <mikaeljnorppa@gmail.com>
2024-02-05 21:50:33 +0200

    asd

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,44 +1,121 @@
 package main;
 import java.util.Scanner;
-
+import java.util.List;
 /**
  * Hello world!
  *
  */
 public class App 
+
 {
 public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("Anna eläintarhalle nimi:");
-        String zooName = sc.nextLine();
-        Zoo zoo = new Zoo(zooName);
+        University university = new University();
 
 
         boolean exit = false;
         while (!exit) {
-            System.out.println("1) Luo uusi eläin, 2) Listaa kaikki eläimet, 3) Juoksuta eläimiä, 0) Lopeta ohjelma");
-            int i = sc.nextInt();
+            System.out.println("1) Lisää opiskelija, 2) Listaa opiskelijat, 3) Lisää opiskelijalle suorite, 4) Listaa opiskelijan suoritteet, 5) Laske opiskelijan suoritusten keskiarvo, 6) Laske opiskelijan suoritusten mediaani, 7) Tallenna opiskelijat tiedostoon, 8) Lataa opiskelijat tiedostosta, 0) Lopeta ohjelma");
+            int choice = sc.nextInt();
 
-            switch(i) {
+            switch(choice) {
                 case 1:
                     sc.nextLine();
-                    System.out.println("Mikä laji?");
-                    String species = sc.nextLine();
-                    System.out.println("Anna eläimen nimi:");
+                    System.out.println("Anna opiskelijan nimi?");
                     String name = sc.nextLine();
-                    System.out.println("Anna eläimen ikä:");
-                    int age = sc.nextInt();
-                    zoo.addAnimal(species, name, age);
+                    System.out.println("Anna opiskelijan opiskelija numero:");
+                    String studentNumber = sc.nextLine();
+                    university.addStudent(new Student(name, studentNumber));
                     break;
                 case 2:
-                    zoo.listAnimals();
+                    List<Student> students = university.getStudents();
+                    System.out.println("Opiskelijat:");
+
+                    for (int i = 0; i < students.size(); i++) {
+                        Student student = students.get(i);
+                        System.out.println(student.getStudentNumber() + ": " + student.getName());
+                    }
                     break;
                 case 3:
-                    System.out.println("Kuinka monta kierrosta?");
-                    int rounds = sc.nextInt();
-                    zoo.runAnimals(rounds);
+                    List<Student> studentsAddGrade = university.getStudents();
+                    for (int i = 0; i < studentsAddGrade.size(); i++) {
+                        Student student = studentsAddGrade.get(i);
+                        System.out.println(i + ": " + student.getName());
+                    }
+                    System.out.println("Mille opiskelijalle suorite lisätään?");
+                    int studentIndex = sc.nextInt();
+                    sc.nextLine();
+                    Student selStudent = university.getStudents().get(studentIndex);
+                    System.out.println("Mille kurssille suorite lisätään?");
+                    String course = sc.nextLine();
+                    System.out.println("Mikä arvosana kurssille lisätään?");
+                    int grade = sc.nextInt();
+                    selStudent.addGrade(course, grade);
                     break;
+                case 4:
+                    List<Student> studentsListGrades = university.getStudents();
+                    for (int i = 0; i < studentsListGrades.size(); i++) {
+                        Student student = studentsListGrades.get(i);
+                        System.out.println(i + ": " + student.getName());
+                    }
+
+                    System.out.println("Minkä opiskelijan suoritteet listataan?");
+                    int studentIndexListGrades = sc.nextInt();
+                    sc.nextLine();
+
+                    if (studentIndexListGrades < 0 || studentIndexListGrades >= studentsListGrades.size()) {
+                        System.out.println("Virheellinen opiskelijanumero.");
+                        break;
+                    }
+
+                    Student selectedStudent = studentsListGrades.get(studentIndexListGrades);
+                    List<Grade> grades = selectedStudent.getGrades();
+
+                    for (Grade gradeItem : grades) {
+                        System.out.println(gradeItem.getCourse() + ": " + gradeItem.getGrade());
+                    }
+                    break;
+                case 5:
+                    List<Student> studentsAverage = university.getStudents();
+                    for (int i = 0; i< studentsAverage.size(); i++) {
+                        Student student = studentsAverage.get(i);
+                        System.out.println(i + ": " + student.getName());
+                    }
+                    System.out.println("Minkä opiskelijan suoritteiden keskiarvo lasketaan?");
+                    int studentIndexAvg = sc.nextInt();
+                    sc.nextLine();
+
+                    if (studentIndexAvg < 0 || studentIndexAvg >= studentsAverage.size()) {
+                        System.out.println("Virheellinen opiskelijanumero.");
+                        break;
+                    }
+                    Student studentAvg = studentsAverage.get(studentIndexAvg);
+                    double average = Calculator.getAvgGrade(studentAvg);
+                    System.out.println("Keskiarvo on " + average);
+                    break;
+                case 6:
+                    List<Student> studentsMedian = university.getStudents();
+                    for (int i = 0; i< studentsMedian.size(); i++) {
+                        Student student = studentsMedian.get(i);
+                        System.out.println(i + ": " + student.getName());
+                    }
+                    System.out.println("Minkä opiskelijan suoritteiden mediaani lasketaan?");
+                    int studentIndexMed = sc.nextInt();
+                    sc.nextLine();
+                    if (studentIndexMed < 0 || studentIndexMed >= studentsMedian.size()) {
+                        System.out.println("Virheellinen opiskelijanumero.");
+                        break;
+                    }
+                    Student studentMed = studentsMedian.get(studentIndexMed);
+                    double median = Calculator.getMedGrade(studentMed);
+                    System.out.println("Mediaani on " + median);
+                    break;
+                case 7:
+                university.saveStudents();
+                break;
+                case 8:
+                university.loadStudents();
+                break;
                 case 0:
                     System.out.println("Kiitos ohjelman käytöstä.");
                     exit = true;
---
+++ src/main/java/main/University.java
@@ -0,0 +1,57 @@
+package main;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class University {
+    private List<Student> students;
+
+    public University() {
+        this.students = new ArrayList<>();
+    }
+    
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public void saveStudents() {
+        try {
+            ObjectOutputStream studentWriter = new ObjectOutputStream(new FileOutputStream("FILENAME"));
+            studentWriter.writeObject(students);
+            studentWriter.close();
+            System.out.println("Opiskelijat tallennettu tiedostoon");
+        } catch (IOException e) {
+            System.out.println("Oppilaitten tallentaminen ei onnistunut");
+            e.printStackTrace();
+        }
+    }
+
+    public void loadStudents() {
+        try {
+            ObjectInputStream studentReader = new ObjectInputStream(new FileInputStream("FILENAME"));
+            students = (ArrayList<Student>) studentReader.readObject();
+            studentReader.close();
+            System.out.println("Opiskelijat ladattu tiedostosta");
+        } catch (FileNotFoundException e) {
+            System.out.println("Oppilaitten lukeminen ei onnistunut");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Oppilaitten lukeminen ei onnistunut");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Oppilaitten lukeminen ei onnistunut");
+            e.printStackTrace();
+        }
+    }       
+
+}
